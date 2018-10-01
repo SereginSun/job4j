186f200b2aa3ac952803c7a11c4ee2d70b3f83a7
SereginSun <sereginsun@yandex.ru>
2018-10-01 19:43:16 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
-
+/**
+ * Calculate
+ *
+ * @author Vladimir Seregin (SereginSun@yandex.ru)
+ *
+ * @version 1.0.1
+ *
+ * @since 01.10.2018
+ */
 public class Calculate{
+	/**
+	 * Main
+	 * @param args - args
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/Package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Package for calculate task.
+ *
+ * @author Vladimir Seregin (SereginSun@yandex.ru)
+ *
+ * @version 1.0.1
+ *
+ * @since 01.10.2018
+ */
+package ru.job4j.calculate;
