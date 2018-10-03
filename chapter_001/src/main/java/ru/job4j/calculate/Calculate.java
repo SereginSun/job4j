f71fa291745790634f860d7ed0a4313e25a91c1f
SereginSun <sereginsun@yandex.ru>
2018-10-03 23:30:37 +0300

    1.5. Checkstyle [#4123]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -8,12 +8,12 @@ package ru.job4j.calculate;
  *
  * @since 01.10.2018
  */
-public class Calculate{
+public class Calculate {
 	/**
 	 * Main
 	 * @param args - args
 	 */
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
 	/**
