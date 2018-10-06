ed18c678fbd9317fb78427903ec0948d04795981
SereginSun <sereginsun@yandex.ru>
2018-10-06 21:42:03 +0300

    add .idea *.iml to .gitignore

--- chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -4,24 +4,24 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ * Test.
+ *
+ * @author Vladimir Seregin (SereginSun@yandex.ru)
+ * @version 1.0.1
+ * @since 03.10.2018
+ */
+public class CalculateTest {
 	/**
-	 * Test.
-	 *
-	 * @author Vladimir Seregin (SereginSun@yandex.ru)
-	 * @version 1.0.1
-	 * @since 03.10.2018
+	 * Test echo.
 	 */
-	public class CalculateTest {
-		 /**
-		  * Test echo.
-		  */
 	@Test
 	public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Vladimir Seregin";
-    String expect = "Echo, echo, echo : Vladimir Seregin"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
+		String input = "Vladimir Seregin";
+		String expect = "Echo, echo, echo : Vladimir Seregin";
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
 	}
- 
+
 }
