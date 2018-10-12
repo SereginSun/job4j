406115d0e004ca17783729d4f8648f9d03864857
SereginSun <sereginsun@yandex.ru>
2018-10-12 23:37:30 +0300

    3.1. Элементарный калькулятор. [#185]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,6 +1,6 @@
 package ru.job4j.calculator;
 /**
- * Calculator
+ * Calculator - this is a class of simple arithmetic operations
  *
  * @author Vladimir Seregin (SereginSun@yandex.ru)
  *
@@ -10,19 +10,63 @@ package ru.job4j.calculator;
  */
 public class Calculator {
     private double result;
-
+    /**
+     * method add
+     *
+     * метод скложения
+     *
+     * @param first - this is the first parametr
+     *
+     * @param second - this is the second parametr
+     */
     public void add(double first, double second) {
         this.result = first + second;
     }
+    /**
+     *  method getResult
+     *
+     * метод возвращает значение поля result.
+     *
+     */
     public double getResult() {
         return this.result;
     }
+
+    /**
+     * method subtract
+     *
+     * разница
+     *
+     * @param first - this is the first parametr
+     *
+     * @param second - this is the second parametr
+     */
     public void subtract(double first, double second) {
         this.result = first - second;
     }
+
+    /**
+     * method div
+     *
+     * частное
+     *
+     * @param first - this is the first parametr
+     *
+     * @param second - this is the second parametr
+     */
     public void div(double first, double second) {
         this.result = first / second;
     }
+
+    /**
+     * method multiple
+     *
+     * произведение параметров first и second
+     *
+     * @param first - this is the first parametr
+     *
+     * @param second - this is the second parametr
+     */
     public void multiple(double first, double second) {
         this.result = first * second;
     }
