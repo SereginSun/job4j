667be6cacfc291ef8ae3f36094e68f18bfd2bbc3
SereginSun <sereginsun@yandex.ru>
2018-10-17 17:19:06 +0300

    4.1. Глупый бот. [#31323]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,23 @@
+package ru.job4j.condition;
+
+/**
+ * @author Vladimir Seregin (SereginSun@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question вопрос от клиента.
+     * @return ответ.
+     */
+    public  String answer(String question){
+        String rs1 = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if("Привет, Бот.".equals(question)){
+            rs1 = "Привет, умник.";
+        } else if("Пока.".equals(question)){
+            rs1 = "До скорой встречи.";
+        }
+        return rs1;
+    }
+}
