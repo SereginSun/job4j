df9e6dfc2b547fa7836cf8cad5941891a2586a2e
SereginSun <sereginsun@yandex.ru>
2018-10-18 23:33:28 +0300

    4.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,27 @@
+package ru.job4j.max;
+/**
+ * @author Vladimir Seregin (SereginSun@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    /**
+     * Метод max возвращает максимальное число из first и second
+     * @param first первое значение
+     * @param second второе значение
+     * @return максимальное значение
+     */
+    public int max(int first, int second){
+        return first < second ? second : first;
+    }
+
+    /**
+     * Метод summation сумму двух значений
+     * @param first первое значение
+     * @param second второе значение
+     * @return сумма
+     */
+    public int summation(int first, int second){
+        return first + second;
+    }
+}
