1bb4b1b331d9b353790a4d15d3906cc556da3f63
SereginSun <sereginsun@yandex.ru>
2018-10-25 20:38:23 +0300

    5.2. Создать программу вычисляющую факториал. [#193]

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -4,17 +4,16 @@ package ru.job4j.loop;
  * Counter
  *
  * @author Seregin Vladimir (SereginSun@yandex.ru)
+ * @version 0.2
  * @since 24.10.2018
- * @version 0.1
  */
 public class Counter {
-    int sum = 0;
     public int add(int start, int finish) {
-        for(int index = start; index <= finish; index++) {
-            if(index % 2 == 0) {
+        int sum = 0;
+        for (int index = start; index <= finish; index++)
+            if (index % 2 == 0) {
                 sum += index;
             }
-        }
         return sum;
     }
 }
--- chapter_001/src/main/java/ru/job4j/loop/Factorial.java
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -1,13 +1,13 @@
 package ru.job4j.loop;
+
 /**
  * Factorial
  *
  * @author Seregin Vladimir (SereginSun@yandex.ru)
- * @since 24.10.2018
  * @version 0.1
+ * @since 24.10.2018
  */
 public class Factorial {
-    int fact = 0;
 
     /**
      * calc
@@ -16,11 +16,10 @@ public class Factorial {
      * @return результат
      */
     public int calc(int n) {
-        for(int index = 0; index <= n; index++) {
+        int fact = 1;
+        for (int index = 1; index <= n; index++) {
             fact *= index;
-            if(index == 0) {
-                fact = 1;
-            }
-        } return fact;
+        }
+        return fact;
     }
 }
