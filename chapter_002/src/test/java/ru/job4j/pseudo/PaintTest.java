46ba39172e66249c80dbed26cf4de42e94289170
SereginSun <sereginsun@yandex.ru>
2018-12-26 19:47:31 +0300

    5. Рефакторинг теста @Before @After [#33568]

--- chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
+++ chapter_002/src/test/java/ru/job4j/pseudo/PaintTest.java
@@ -1,12 +1,14 @@
 package ru.job4j.pseudo;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author Seregin Vladimir (SereginSun@yandex.ru);
@@ -14,13 +16,25 @@ import static org.junit.Assert.*;
  * @since 0.1
  */
 public class PaintTest {
+    private final PrintStream stdout = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Square());
-        assertThat(new String(out.toByteArray()),is(new StringBuilder()
+        assertThat(new String(this.out.toByteArray()),is(new StringBuilder()
                 .append("++++")
                 .append("+  +")
                 .append("+  +")
@@ -29,16 +43,12 @@ public class PaintTest {
                 .toString()
                 )
         );
-    System.setOut(stdout);
     }
 
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().draw(new Triangle());
-        assertThat(new String(out.toByteArray()), is(new StringBuilder()
+        assertThat(new String(this.out.toByteArray()), is(new StringBuilder()
                 .append("   +   ")
                 .append("  + +  ")
                 .append(" +   + ")
@@ -47,6 +57,5 @@ public class PaintTest {
                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
 }
