952d8d15a7b19bfcf68c06c3e3d33a1aa14f6da2
SereginSun <sereginsun@yandex.ru>
2019-01-20 09:09:58 +0300

    3. if-else-throw [#49895]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -1,7 +1,6 @@
 package ru.job4j.tracker;
 
 /**
- *класс StartUI
  * @author Vladimir Seregin (SereginSun@yandex.ru)
  * @version 1.0.2
  * @since 14.12.2018
@@ -29,10 +28,9 @@ public class ConsoleInput implements Input {
                 break;
             }
         }
-        if(exist) {
-            return key;
-        } else {
+        if(!exist) {
             throw new MenuOutException("Out of menu range.");
         }
+        return key;
     }
 }
