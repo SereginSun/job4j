b631c9f25008ec0bd06915aa593c5580dd98daa5
SereginSun <SereginSun@yandex.ru>
2019-03-15 09:21:04 +0300

    Кофемашина. [#34741] corrected code

--- chapter_002/src/main/java/ru/job4j/coffeemachine/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/coffeemachine/CoffeeMachine.java
@@ -18,20 +18,24 @@ public class CoffeeMachine {
      * @param price - цена кофе
      * @return массив монет
      */
-    public int[] changes (int value, int price) {
+    public int[] changes(int value, int price) {
         int change = value - price;
         int count = 0;
-         for (int i = 0; i < 4;) {
+         for (int i = 0; i < 4; i++) {
              int quotient = change / coins[i];
              if (quotient >= 1) {
-                 for (int j = 0; j < quotient; j++) {
+                 int j = 0;
+                 while (j < quotient) {
                      if (change - coins[i] >= 0) {
                          change -= coins[i];
                          smallChange[count] = coins[i];
                          count++;
-                     } else i++;
+                     } else {
+                         i++;
+                     }
+                     j++;
                  }
-             } else i++;
+             }
          }
         return Arrays.copyOf(smallChange, count);
     }
