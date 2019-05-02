682fa13b3effd7c8f762444ecd8692bb1b9bd684
SereginSun <sereginsun@yandex.ru>
2019-05-02 21:55:26 +0300

    1. Фильтрация учеников. [#110058]

---
+++ chapter_004/src/main/java/stream/School.java
@@ -0,0 +1,24 @@
+package stream;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+/**
+ * class School
+ * @author Vladimir Seregin (SereginSun@yandex.ru)
+ * @version $Id$
+ * @since 02.05.2019
+ */
+public class School {
+
+    /**
+     * This method distributes pupils into classes according to academic achievement.
+     * @param student - List of student.
+     * @param predicate - Predicate (Student's score)
+     * @return Students that fit on predicate.
+     */
+    List<Student> collect(List<Student> student, Predicate<Student> predicate) {
+        return student.stream().filter(predicate).collect(Collectors.toList());
+    }
+}
---
+++ chapter_004/src/main/java/stream/package-info.java
@@ -0,0 +1,9 @@
+package stream;
+
+/**
+ * Package for the distribution of students in the respective classes, depending according to academic achievementю
+ *
+ * @author Vladimir Seregin (SereginSun@yandex.ru)
+ * @version $Id$
+ * @since 02.05.2019
+ */
