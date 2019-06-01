5f780a893a3c8ba07399a22fbfdfcf91a28a1093
SereginSun <sereginsun@yandex.ru>
2019-06-01 17:09:20 +0300

    5.1.1. Итератор для двухмерного массива int[][][#145657] removed array counting

--- chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
+++ chapter_005/src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -10,42 +10,24 @@ import java.util.Iterator;
  * @since 31.05.2019
  */
 public class MatrixIterator implements Iterator {
-    private int size;
-    private int position = 0;
+    private final int[][] values;
     private int row = 0;
     private int column = 0;
-    private final int[][] values;
 
     public MatrixIterator(final int[][] values) {
-        this.size = countElement(values);
         this.values = values;
-    }
-
-    /**
-     * The method counts the number of elements in the matrix.
-     *
-     * @param values - integer matrix.
-     * @return number of elements in the matrix.
-     */
-    private int countElement(final int[][] values) {
-        int count = 0;
-        for (int[] row : values) {
-            count += row.length;
-        }
-        return count;
     }
 
     @Override
     public boolean hasNext() {
-        return size > position;
+        return this.row < this.values.length && this.column < this.values[row].length;
     }
 
     @Override
     public Object next() {
         Object element = values[row][column];
-        position++;
         column++;
-        while (row < values.length && column >= values[row].length) {
+        if (this.column >= this.values[row].length) {
             column = 0;
             row++;
         }
