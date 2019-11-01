f0bc93d309187afcd4faa9d2f47376a5c86720c9
SereginSun <sereginsun@yandex.ru>
2019-11-01 23:02:27 +0300

    Почта. [#184458] changed the decision - made through a reversing map, added tests.

--- chapter_006/src/main/java/ru/job4j/post/Merger.java
+++ chapter_006/src/main/java/ru/job4j/post/Merger.java
@@ -1,5 +1,6 @@
 package ru.job4j.post;
 
+import java.io.*;
 import java.util.*;
 
 /**
@@ -10,32 +11,74 @@ import java.util.*;
  * @since 12.10.2019
  */
 public class Merger {
+    private Map<String, String> mapIn = new HashMap<>();
+    private Map<String, Set<String>> mapOut = new HashMap<>();
+
+    /**
+     * This method starts application execution.
+     */
+    public void run() {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String message = br.readLine();
+            while (!"finish".equals(message) && message != null) {
+                String userName = message;
+                message = br.readLine();
+                if (message.length() != 0 && message.matches("^(.+)@(.+)$")) {
+                    String[] listEmail = message.split(", ");
+                    Set<String> emailHashSet = new HashSet<>(Arrays.asList(listEmail));
+                    User user = new User(userName, emailHashSet);
+                    String oldUser = null;
+                    for (String email : user.getEmail()) {
+                        if (mapIn.get(email) == null) {
+                            this.mapIn.put(email, userName);
+                        } else {
+                            oldUser = mapIn.get(email);
+                        }
+                    }
+                    if (oldUser != null) {
+                        for (String email : emailHashSet) {
+                            this.mapIn.put(email, oldUser);
+                        }
+                    }
+                }
+                message = br.readLine();
+            }
+            mergeUsersByEmail();
+            writeResultLog();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * This method implements an algorithm that merges users who have the same email.
-     *
-     * @param userList - list of users, each of which has a list of emails.
-     * @return a list of united users, each of which has a list of emails.
      */
-    public List<User> mergeUsersByEmail(List<User> userList) {
-        List<User> result = new ArrayList<>();
-        result.add(userList.get(0));
+    public void mergeUsersByEmail() {
 
-        for (User in : userList) {
-            boolean flag = true;
-
-            for (User out : result) {
-                if (!Collections.disjoint(out.getEmail(), in.getEmail())) {
-                    out.getEmail().addAll(in.getEmail());
-                    flag = false;
-                }
-            }
-
-            if (flag) {
-                result.add(in);
+        for (Map.Entry<String, String> entry : mapIn.entrySet()) {
+            Set<String> emailSet = new HashSet<>();
+            if (mapOut.get(entry.getValue()) == null) {
+                emailSet.clear();
+                emailSet.add(entry.getKey());
+                mapOut.put(entry.getValue(), emailSet);
+            } else {
+                emailSet = mapOut.get(entry.getValue());
+                emailSet.add(entry.getKey());
+                mapOut.put(entry.getValue(), emailSet);
             }
         }
+    }
 
-        return result;
+    /**
+     * This method writes merged users by e-mail to the console.
+     */
+    public void writeResultLog() {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            for (Map.Entry<String, Set<String>> entry : mapOut.entrySet()) {
+                bw.write(entry.getKey() + '\n' + entry.getValue() + '\n');
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ chapter_006/src/test/java/ru/job4j/post/MergerTest.java
@@ -0,0 +1,68 @@
+package ru.job4j.post;
+
+import com.google.common.base.Joiner;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MergerTest {
+    private ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private static final String newLine = System.getProperty("line.separator");
+
+    private String line1 = "user1";
+    private String line2 = "xxx@ya.ru, foo@gmail.com, lol@mail.ru";
+    private String line3 = "user2";
+    private String line4 = "foo@gmail.com, ups@pisem.net";
+    private String line5 = "user3";
+    private String line6 = "xyz@pisem.net, vasya@pupkin.com";
+    private String line7 = "user4";
+    private String line8 = "ups@pisem.net, aaa@bbb.ru";
+    private String line9 = "user5";
+    private String line10 = "xyz@pisem.net";
+
+    private String text = Joiner.on(newLine).join(
+            line1, line2, line3, line4, line5, line6, line7, line8, line9, line10
+    );
+
+    private ByteArrayInputStream in = new ByteArrayInputStream(text.getBytes());
+
+    @Before
+    public void setUp() {
+        System.setIn(in);
+        System.setOut(new PrintStream(output));
+    }
+
+    @Test
+    public void mergeUsersByEmail() {
+        Merger merger = new Merger();
+        merger.run();
+
+        Map<String, Set<String>> expected = new HashMap<>();
+        expected.put("user1", Set.of("aaa@bbb.ru, ups@pisem.net, lol@mail.ru, xxx@ya.ru, foo@gmail.com"));
+        expected.put("user3", Set.of("vasya@pupkin.com, xyz@pisem.net"));
+
+        StringBuilder sb = new StringBuilder();
+
+        for (Map.Entry<String, Set<String>> entry : expected.entrySet()) {
+            sb.append(entry.getKey());
+            sb.append("\n");
+            sb.append(entry.getValue());
+            sb.append("\n");
+        }
+
+        String textExpected = sb.toString();
+
+        String result = output.toString().replaceAll("\r", "");
+
+        assertThat(result, is(textExpected));
+    }
+}
