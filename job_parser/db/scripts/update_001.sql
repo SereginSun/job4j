e7ecf114c36763eed6432aa19d1513db18fb1b5f
SereginSun <sereginsun@yandex.ru>
2020-06-13 22:36:12 +0300

    Made some changes to the project.

--- job_parser/db/scripts/update_001.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists vacancy(
-             id SERIAL PRIMARY KEY NOT NULL,
-             name VARCHAR (1000) UNIQUE,
-             text TEXT,
-             link TEXT,
-             date TIMESTAMP
-);
